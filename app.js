2f3ee3caa3553f3cde4cd0c179123814d2bfbd30
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-04-12 15:53:56 +0530

    Express Generator

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
-var leaderRouder = require('./routes/leaderRouter');
+var leaderRouter = require('./routes/leaderRouter');
 var promoRouter = require('./routes/promoRouter');
 
 var app = express();
@@ -26,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
-app.use('/leaders',leaderRouder);
+app.use('/leaders',leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
